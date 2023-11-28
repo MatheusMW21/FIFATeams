b1bc7bfc27f58d912825f3c4f2993dcec878f53e
matheus <matheusmw21@gmail.com>
2023-11-28 10:22:04 -0300

    sorteando dois time

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const timesFIFA = ["Barcelona", "Real Madrid", "Manchester City"];
+const timesFIFA = ["Barcelona", "Real Madrid", "Manchester City", "Manchester United",
+"Borussia Dortmund"];
 
 function sortearTime() {
-    const indiceSorteado = Math.floor(Math.random() * timesFIFA.length);
-    const timeSorteado = timesFIFA[indiceSorteado];
-    document.getElementById("timeSorteado").innerText = `Time Sorteado: {timeSorteado}`;
+    let indiceSorteado1 = Math.floor(Math.random() * timesFIFA.length);
+    let indiceSorteado2;
+
+    do {
+        indiceSorteado2 = Math.floor(Math.random() * timesFIFA.length);
+    } while (indiceSorteado2 === indiceSorteado1);
+
+    const timeSorteado1 = timesFIFA[indiceSorteado1];
+    const timeSorteado2 = timesFIFA[indiceSorteado2];
+
+    const result = document.getElementById("timeSorteado");
+    result.innerHTML = `Times Sorteados: ${timeSorteado1} vs ${timeSorteado2}`;
 }
